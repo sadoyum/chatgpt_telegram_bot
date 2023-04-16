47e107fa2e924a165fbeb351e3896d0b484f2b5a
sadoyum <92607984+sadoyum@users.noreply.github.com>
2023-04-16 17:02:40 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -12,10 +12,10 @@ with open(config_dir / "config.yml", 'r') as f:
 config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
-telegram_token = config_yaml["telegram_token"]
-openai_api_key = config_yaml["openai_api_key"]
+telegram_token = config_yaml["5818925590:AAGCTIR1wQqqtEhSqOYqZslKGp3JQ-DumHo"]
+openai_api_key = config_yaml["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"]
 use_chatgpt_api = config_yaml.get("use_chatgpt_api", True)
-allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
+allowed_telegram_usernames = config_yaml["hurkole"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
 enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
